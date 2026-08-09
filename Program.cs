f06a2dabd6d3c76f4e872b2ac617f3c4574f259a
Mehak Chopra <Mehak.Chopra@ccam-va.com>
2026-08-09 15:52:31 -0400

    completed the difficulty task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TileCostCalculator
+﻿using System.Reflection.Metadata;
+
+namespace TileCostCalculator
 {
     internal class Program
     { 
@@ -7,19 +9,42 @@
             double length = 1.0;
             double width = 1.0;
             double costPerFlooringUnit = 1.0;
-            double area = length * width;
+            double area = 1.0; // is it better to use one variable for circle and rectangle areas or separate variables?
+            double radius = 1.0;
+            double circleArea = Math.PI * radius * radius;
+            int avgFlooringFootagePerHour = 20;
+            double costPerHour = 86.00;
 
-            Console.WriteLine ("Enter room length: ");
-            length = Convert.ToDouble(Console.ReadLine());
+            // ask user for shape of the room: rectangle or circle
+            Console.Write("Please enter the shape of the room (rectangle or circle): ");
+            string shape = Console.ReadLine();
 
-            Console.WriteLine ("Enter room width: ");
-            width = Convert.ToDouble(Console.ReadLine());
+            if (shape == "rectangle")
+            {
+                Console.Write("Enter room length in feet: ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter room width in feet: ");
+                width = Convert.ToDouble(Console.ReadLine());
+                area = length * width;
 
-            area = length * width;
-            Console.WriteLine($"Room area: {area}");
+            }
+            else if (shape == "circle")
+            {
+                Console.Write("Radius of the circle in feet: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+                area = Math.PI * radius * radius;
 
-            Console.WriteLine ("Enter cost per flooring unit: ");
+            }
+            else Console.WriteLine("Shape not recognized. Please enter either 'rectangle' or 'circle'.");
+
+            Console.WriteLine($"Room area: {area} square feet");
+
+            Console.Write ("Enter cost per flooring unit: ");
             costPerFlooringUnit = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine ("Cost to cover area with flooring: " + (area * costPerFlooringUnit));
+
+            Console.WriteLine ("Cost of labor to install flooring: " + (area / avgFlooringFootagePerHour * costPerHour));
 
         }
     }
