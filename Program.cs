e137a998b27d5a391c0ee869b630538d106c7bd7
Mehak Chopra <Mehak.Chopra@ccam-va.com>
2026-08-09 15:16:23 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TileCostCalculator
+{
+    internal class Program
+    { 
+        static void Main()
+        {
+            double length = 1.0;
+            double width = 1.0;
+            double costPerFlooringUnit = 1.0;
+            double area = length * width;
+
+            Console.WriteLine ("Enter room length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine ("Enter room width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+
+            area = length * width;
+            Console.WriteLine($"Room area: {area}");
+
+            Console.WriteLine ("Enter cost per flooring unit: ");
+            costPerFlooringUnit = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
