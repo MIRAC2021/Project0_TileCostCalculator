131ff44e94150f94f8446eb71cc08cabecf1fa2c
Mehak Chopra <Mehak.Chopra@ccam-va.com>
2026-08-09 16:02:28 -0400

    completed project

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace TileCostCalculator
             double area = 1.0; // is it better to use one variable for circle and rectangle areas or separate variables?
             double radius = 1.0;
             double circleArea = Math.PI * radius * radius;
-            int avgFlooringFootagePerHour = 20;
-            double costPerHour = 86.00;
+            int AVERAGEFLOORINGFOOTAGEPERHOUR = 20;
+            double LABORCOSTPERHOUR = 86.00;
 
             // ask user for shape of the room: rectangle or circle
             Console.Write("Please enter the shape of the room (rectangle or circle): ");
-            string shape = Console.ReadLine();
+            string shape = Console.ReadLine();  //will this if/else statement be enough to hadle null input or should I add a check for null input?
 
             if (shape == "rectangle")
             {
@@ -35,7 +35,11 @@ namespace TileCostCalculator
                 area = Math.PI * radius * radius;
 
             }
-            else Console.WriteLine("Shape not recognized. Please enter either 'rectangle' or 'circle'.");
+            else
+            {
+                Console.WriteLine("Shape not recognized. Please enter either 'rectangle' or 'circle' next time.");  //Is there a way I can make the code go back to the original "if" statement instead of exiting the program? I want to give the user another chance to enter a valid shape.
+                Environment.Exit(0);
+            }
 
             Console.WriteLine($"Room area: {area} square feet");
 
@@ -44,7 +48,7 @@ namespace TileCostCalculator
 
             Console.WriteLine ("Cost to cover area with flooring: " + (area * costPerFlooringUnit));
 
-            Console.WriteLine ("Cost of labor to install flooring: " + (area / avgFlooringFootagePerHour * costPerHour));
+            Console.WriteLine ("Cost of labor to install flooring: " + (area / AVERAGEFLOORINGFOOTAGEPERHOUR * LABORCOSTPERHOUR));
 
         }
     }
